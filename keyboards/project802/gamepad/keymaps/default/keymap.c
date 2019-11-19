59f6bdee449b73ad08f4be2fc01778cbd9bcbb1a
project802 <chris@project802.net>
2019-11-18 19:10:40 -0500

    Update gamepad to include G mapping

--- keyboards/project802/gamepad/keymaps/default/keymap.c
+++ keyboards/project802/gamepad/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = {
 			{ RESET,    KC_1,    KC_2,    KC_3,    KC_4,    KC_O },
 			{ KC_TAB,   KC_Q,    KC_UP,   KC_E,    KC_R,    KC_M },
-			{ KC_CAPS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_F,    KC_F1 },
+			{ KC_CAPS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_G,    KC_F1 },
 			{ KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX },
 			{ KC_LCTRL, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX }
 		}
