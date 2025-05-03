e8cf25dd2ae49b3a0406f2eb3e2940c30c3a6b15
Chris Vincent <project802@users.noreply.github.com>
2025-05-02 20:14:13 -0400

    Initial commit of Jiggler

---
+++ keyboards/project802/jiggler/jiggler.c
@@ -0,0 +1,62 @@
+#include "quantum.h"
+#include "print.h"
+
+#define PRESS_MAX_MS (2 * 1000)
+#define WAIT_MAX_MS (60 * 1000)
+
+typedef enum {
+    JIGGLER_STATE_IDLE,
+    JIGGLER_STATE_PRESSED,
+} eJigglerState;
+
+static deferred_token jiggler_token = INVALID_DEFERRED_TOKEN;
+
+uint32_t jiggler_callback( uint32_t trigger_time, void *cb_arg )
+{
+    static eJigglerState state = JIGGLER_STATE_IDLE;
+    static report_mouse_t report = { 0 };
+    
+    // rand() returns an int but since we are looking for a positive
+    // time interval only, cast it to unsigned
+    unsigned int retVal = (unsigned int) rand();
+    unsigned int randShift = 0;
+    
+    do
+    {
+        randShift = ((unsigned int) rand()) & 0b111;
+    }
+    while( randShift == 0 );
+    
+
+    uprintf( "jiggler_callback enter: state=%u\n", state );
+
+    switch( state )
+    {
+        case JIGGLER_STATE_IDLE:
+            report.buttons = MOUSE_BTN8;
+            state = JIGGLER_STATE_PRESSED;
+            while( retVal > PRESS_MAX_MS ) retVal = retVal >> randShift;
+            break;
+        
+        case JIGGLER_STATE_PRESSED:
+            report.buttons = 0;
+            state = JIGGLER_STATE_IDLE;
+            while( retVal > WAIT_MAX_MS ) retVal = retVal >> randShift;
+            break;
+            
+        default:
+            state = JIGGLER_STATE_IDLE;
+            break;
+    }
+
+    host_mouse_send( &report );
+
+    uprintf( "jiggler_callback exit: state=%u retVal=%u\n", state, retVal );
+    
+    return (uint32_t) retVal;
+}
+
+void keyboard_post_init_user( void )
+{
+    jiggler_token = defer_exec( 1500, jiggler_callback, NULL );
+}
